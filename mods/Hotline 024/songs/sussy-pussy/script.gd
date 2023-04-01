d5cbc79562c8a158bd08b5c5573b847a6ace9cb7
ImCodist <50346006+ImCodist@users.noreply.github.com>
2023-04-01 18:17:06 -0400

    ok no moew

--- mods/Hotline 024/songs/sussy-pussy/script.gd
+++
@@ -1,9 +0,0 @@
-extends FNFScript
-
-func _process(_delta):
-    if play_state.player_strum is Node:
-        for _button in play_state.player_strum.get_children():
-            var _notes_node = _button.get_node("Notes")
-
-            for _note in _notes_node.get_children():
-                _note.rotation_degrees += 10 * _delta
