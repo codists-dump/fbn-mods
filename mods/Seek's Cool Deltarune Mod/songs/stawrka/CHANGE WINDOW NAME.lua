ee318fc50b11c1c349c0e50aefd0981678b2c0d2
ImCodist <50346006+ImCodist@users.noreply.github.com>
2023-04-03 00:59:21 -0400

    fixed things

--- mods/Seek's Cool Deltarune Mod/songs/stawrka/CHANGE WINDOW NAME.lua
+++
@@ -1,9 +0,0 @@
-function onCreate()
-    setPropertyFromClass("openfl.Lib", "application.window.title",'The Original         Starwalker')
-end
-
-function onUpdate()
-    if getProperty("health") < 0 then
-        setPropertyFromClass("openfl.Lib", "application.window.title",'DARKNESS')
-    end
-end
