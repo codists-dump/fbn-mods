c17fdab3fe01ab51b43145b4ce13e7ab0be7a031
codist <50346006+ImCodist@users.noreply.github.com>
2023-05-31 15:37:52 -0400

    Add files via upload

---
+++ mods/Other/songs/lore-awesomix/tester.lua
@@ -0,0 +1,11 @@
+
+function onUpdate()
+    setProperty('healthBar.alpha', 0)
+    setProperty('healthBarBG.alpha', 0)
+    setProperty('iconP1.alpha', 0)
+    setProperty('iconP2.alpha', 0)
+    setProperty('scoreTxt.alpha', 0)
+    setProperty('timeBar.alpha', 0)
+    setProperty('timeBarBG.alpha', 1)
+    setProperty('timeTxt.alpha', 1)
+end
