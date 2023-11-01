13909c125988dc0b8e3a050aab719028b8805e01
codist <50346006+ImCodist@users.noreply.github.com>
2023-10-31 23:42:51 -0400

    Update script.gd

--- characters/tzen-bf/script.gd
+++ characters/tzen-bf/script.gd
@@ -11,7 +11,12 @@ func _ready():
 	character.animation_player.connect("animation_finished", self, "_on_animation_finished")
 
 func _on_taunt(_alt_key):
-	character.play("hey", 1)
+	if _alt_key:
+		if character.anim_prefix == "":
+			character.play("hey-alt", 1)
+		else:
+			character.play("hey", 1)
+		
 
 func _on_animation_finished(_anim):
 	if _anim == "hey-alt":
