e3347930f3c800a0d48152bd8c06cf5377276832
codist <50346006+ImCodist@users.noreply.github.com>
2023-10-31 22:30:20 -0400

    Update script.gd

--- characters/tzen-bf/script.gd
+++ characters/tzen-bf/script.gd
@@ -1,11 +1,8 @@
 extends FNFScript
 
+
 func _get_features():
 	return ["characters/alt_animation"]
 
 func _loaded():
 	run_feature("characters/alt_animation", "register_character", [character])
-
-func _on_animation_finished(_anim):
-	if _anim == "hey-alt":
-		character.play("mic", 1)
