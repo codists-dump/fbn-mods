ee4645cea0eda0f944ba33917b048e5ed1c9bc82
codist <50346006+ImCodist@users.noreply.github.com>
2023-10-31 22:28:24 -0400

    Updated tzen bf to use feature system.

--- characters/tzen-bf/script.gd
+++ characters/tzen-bf/script.gd
@@ -1,19 +1,10 @@
 extends FNFScript
 
-func _ready():
-	character.connect("taunted", self, "_on_taunt")
-	character.animation_player.connect("animation_finished", self, "_on_animation_finished")
+func _get_features():
+	return ["characters/alt_animation"]
 
-func _on_taunt(_alt_key):
-	character.do_taunt_sound = not _alt_key
-
-	if _alt_key:
-		if character.anim_prefix == "":
-			character.anim_prefix = "-alt"
-		else:
-			character.anim_prefix = ""
-	
-	character.play("hey", 1)
+func _loaded():
+	run_feature("characters/alt_animation", "register_character", [character])
 
 func _on_animation_finished(_anim):
 	if _anim == "hey-alt":
