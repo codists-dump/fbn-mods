1fb7f10fe419d4674e3589f7783b52e9e3512e10
codist <50346006+ImCodist@users.noreply.github.com>
2023-10-31 22:24:32 -0400

    Add files via upload

---
+++ features/characters/alt_animation.gd
@@ -0,0 +1,15 @@
+extends FNFScript
+
+func register_character(character):
+	character.connect("taunted", self, "_on_taunt", [character])
+
+func _on_taunt(_alt_key, _character):
+	_character.do_taunt_sound = not _alt_key
+	
+	if _alt_key:
+		if _character.anim_prefix == "":
+			_character.anim_prefix = "-alt"
+			_character.play("transALTin", 1)
+		else:
+			_character.anim_prefix = ""
+			_character.play("transALTout", 1)
